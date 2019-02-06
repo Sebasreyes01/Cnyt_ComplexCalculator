9f41724d449019d762462193e0808a3af390b0ae
Sebastián Reyes <sebastianreyes01@hotmail.com>
2019-02-06 11:28:41 -0500

    ComplexNumber class documented

--- src/main/java/com/edu/eci/ieti/complexCalculator/ComplexNumber.java
+++ src/main/java/com/edu/eci/ieti/complexCalculator/ComplexNumber.java
@@ -7,19 +7,36 @@ public class ComplexNumber {
 	private double realNumber;
 	private double imaginaryNumber;
 
+	/**
+	 * Constructor of the class
+	 * @param realNumber is the real part of the complex number.
+	 * @param imaginaryNumber is the imaginary part of the complex number.
+	 */
 	public ComplexNumber(double realNumber, double imaginaryNumber) {
 		this.realNumber = realNumber;
 		this.imaginaryNumber = imaginaryNumber;
 	}
 
+	/**
+	 * Gets the real part of the complex number.
+	 * @return the real part of the complex number.
+	 */
 	public double getRealNumber() {
 		return realNumber;
 	}
 
+	/**
+	 * Gets the imaginary part of the complex number.
+	 * @return the imginary part of the complex number.
+	 */
 	public double getImaginaryNumber() {
 		return imaginaryNumber;
 	}
 
+	/**
+	 * Displays the complex number in a string.
+	 * @return the complex number in a string.
+	 */
 	public String getComplexNumber() {
 		DecimalFormat df = new DecimalFormat("#.##");
 		if(imaginaryNumber < 0) {
@@ -29,18 +46,35 @@ public class ComplexNumber {
 		}
 	}
 
+	/**
+	 * Calculates the modulus of the complex number.
+	 * @return the modulus of the complex number.
+	 */
 	public double modulus() {
 		return  Math.sqrt(Math.pow(realNumber, 2) + Math.pow(imaginaryNumber, 2));
 	}
 
+	/**
+	 * Calculates the conjugate of the complex number.
+	 * @return the conjugate of the complex number.
+	 */
 	public ComplexNumber conjugate() {
 		return new ComplexNumber(realNumber, imaginaryNumber * -1);
 	}
 
+	/**
+	 * Gets the phase of the complex number.
+	 * @return the phase of the complex number.
+	 */
 	public double getPhase() {
 		return Math.atan2(imaginaryNumber, realNumber);
 	}
 
+	/**
+	 * Compares two complex numbers and determines if are equal.
+	 * @param o the object to be compered with.
+	 * @return if the complex number is equal to the object.
+	 */
 	@Override
 	public boolean equals(Object o) {
 		if (o == this) {
